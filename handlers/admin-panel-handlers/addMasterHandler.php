30f4f57fd916d99c459da5884d0bec1ec69b6d79
valerykosss <valery,kosss@gmail.com>
2024-04-20 04:15:35 +0300

    new variant add master(admin panel) check add and delete before page reload

--- handlers/admin-panel-handlers/addMasterHandler.php
+++ handlers/admin-panel-handlers/addMasterHandler.php
@@ -3,19 +3,50 @@
     if (session_id() == '')
     session_start();
 
-        $master_name = $_POST['master_name'];
-        $master_surname = $_POST['master_surname'];
-        $master_photo = $_POST['master_photo'];
-        $education = $_POST['education'];
-        $work_experience = $_POST['work_experience'];
-        $position = $_POST['position'];
+    //     $master_name = $_POST['master_name'];
+    //     $master_surname = $_POST['master_surname'];
+    //     $master_photo = $_POST['master_photo'];
+    //     $education = $_POST['education'];
+    //     $work_experience = $_POST['work_experience'];
+    //     $position = $_POST['position'];
 
 
-        $queryToInsert = mysqli_query($link, "INSERT INTO master (master_name, master_surname, master_photo, education, work_experience, position) 
-        VALUES ('$master_name', '$master_surname', '$master_photo', '$education', '$work_experience', '$position')");
+    //     $queryToInsert = mysqli_query($link, "INSERT INTO master (master_name, master_surname, master_photo, education, work_experience, position) 
+    //     VALUES ('$master_name', '$master_surname', '$master_photo', '$education', '$work_experience', '$position')");
 
-        echo $queryToInsert;
+    //     echo $queryToInsert;
    
-    require '../../partials/table-master-admin-panel.php';
+    // require '../../partials/table-master-admin-panel.php';
+
+    $master_name = $_POST['master_name'];
+    $master_surname = $_POST['master_surname'];
+    $master_photo = $_POST['master_photo'];
+    $education = $_POST['education'];
+    $work_experience = $_POST['work_experience'];
+    $position = $_POST['position'];
+
+    // Подготавливаем SQL-запрос для добавления мастера
+    $query = "INSERT INTO `master` (`master_name`, `master_surname`, `master_photo`, `education`, `work_experience`, `position`) 
+            VALUES ('$master_name', '$master_surname', '$master_photo', '$education', '$work_experience', '$position')";
+
+    if (mysqli_query($link, $query)) {
+        $masterId = mysqli_insert_id($link); // Получаем ID нового мастера
+        $response = [
+            'success' => true,
+            'master' => [
+                'id' => $masterId,
+                'master_name' => $master_name,
+                'master_surname' => $master_surname,
+                'master_photo' => $master_photo,
+                'education' => $education,
+                'work_experience' => $work_experience,
+                'position' => $position
+            ]
+        ];
+        echo json_encode($response);
+    } else {
+        $response = ['success' => false];
+        echo json_encode($response);
+    }
     
 ?>
