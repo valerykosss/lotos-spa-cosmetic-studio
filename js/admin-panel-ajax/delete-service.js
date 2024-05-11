3c5477f2b9a263183bdcd5d1dc963638528630b9
OlegPazny <petr.jakimovets@gmail.com>
2024-05-11 19:11:24 +0300

    удаление услуг

--- js/admin-panel-ajax/delete-service.js
+++ js/admin-panel-ajax/delete-service.js
@@ -1,23 +1,23 @@
 //удаление мастера
 $(document).on('click', '.delete-service__button', function () {
-    // let id_master_to_delete = $(this).attr('id');
-    // deleteMaster(id_master_to_delete, $(this));
+    let id_service_to_delete = $(this).attr('id');
+    deleteService(id_service_to_delete, $(this));
 });
 
-function deleteMaster(id_master_to_delete, button) {
-    // $.ajax({
-    //     url: '../handlers/admin-panel-handlers/deleteMasterHandler.php',
-    //     type: 'POST',
-    //     data: {
-    //         id_master_to_delete: id_master_to_delete
-    //     },
-    //     success: function (response) {
-    //         let masterRow = button.closest('tr'); // Используем closest для поиска ближайшего <tr>
-    //         masterRow.remove();
-    //         alert("Мастер удален!");
-    //     },
-    //     error: function () {
-    //         console.error('Произошла ошибка при удалении пользователя');
-    //     }
-    // });
+function deleteService(id_service_to_delete, button) {
+    $.ajax({
+        url: '../handlers/admin-panel-handlers/deleteServiceHandler.php',
+        type: 'POST',
+        data: {
+            id_service_to_delete: id_service_to_delete
+        },
+        success: function (response) {
+            let serviceRow = button.closest('tr'); // Используем closest для поиска ближайшего <tr>
+            serviceRow.remove();
+            alert("Услуга удалена!");
+        },
+        error: function () {
+            console.error('Произошла ошибка при удалении услуги');
+        }
+    });
 }
