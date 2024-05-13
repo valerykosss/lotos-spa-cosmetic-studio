cb6f90660c62114b43e7a6890916b31375cbdaaa
valerykosss <valery.kosss@gmail.com>
2024-05-14 00:48:36 +0300

    мое подключение бд и мои стили хедера

--- database/db.php
+++ database/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $database = "lotos";
 $user = "root";
-$password = "";
+$password = "root";
 
 $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка" . mysqli_error($link));
 ?>
