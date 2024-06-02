a7024a6e7deed84aa195b6b25b3cf77ef1de3507
valerykosss <valery.kosss@gmail.com>
2024-06-03 01:26:33 +0300

    каталоги типов сервиса сверстаны

--- partials/serviceCards.php
+++ partials/serviceCards.php
@@ -4,11 +4,13 @@
         $rows = mysqli_num_rows($result); 
         for ($i = 0; $i < $rows; ++$i) {
             $row = mysqli_fetch_row($result); 
+            //
             echo "<div class=\"card-service__body\">";
-                echo "<img class=\"service-img\" src=\"" .$row[3]. "\" alt=\"\">";
+            echo "<div class=\"service-img-block\">";
+                echo "<img class=\"service-img\" src=\"" .$row[3]. "\" alt=\"\">"; echo "</div>";
                 echo "<p class=\"service-name\">".$row[2]."</p>";
-                echo "<p style='margin-bottom: 30px;' class=\"service-description\">".$row[4]."</p>";
-                echo "<a style='display: flex; justify-content: center;'href='service-page.php?service_id=".$row[0]."'>";
+                echo "<p class=\"service-description\">".$row[4]."</p>";
+                echo "<a href='service-page.php?service_id=".$row[0]."'>";
                     echo "<div class=\"service-button button\" id=\"".$row[0]."\">";
                         echo "<span class=\"details\">ПОДРОБНЕЕ</span>";
                     echo "</div>";
